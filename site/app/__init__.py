6890ebc5bba4a55a8e14b2e93964e30371eeddfe
Mantas Andriekus <mantas.andriekus@gmail.com>
2024-02-22 21:48:42 +0200

    Added connection to MySQL database

--- site/app/__init__.py
+++ site/app/__init__.py
@@ -1,19 +1,26 @@
 """ This file is used to make most common actions """
+import boto3
+import json
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
-
+def get_secrets():
+    client = boto3.client('secretsmanager', reion_name='eu-central-1')
+    response = client.get_secret_value(SecretId='db_creds')
+    secrets = json.loads(response['SecretString'])
+    return secrets
 # initialize SQLAlchemy object to
 # communication with database
 db = SQLAlchemy()
-DB = "my.db"
+aws_secrets = get_secrets()
 
 def create_app():
     """ using onw method to create application"""
     app = Flask(__name__)
-    app.config["SECRET_KEY"] = "sdfsdfs"
-    app.config["SQLALCHEMY_DATABASE_URI"] = f'sqlite:///{DB}'
+    app.config["SECRET_KEY"] = aws_secrets['flask_secret_key']
+    app.config["SQLALCHEMY_DATABASE_URI"] = \
+    f'mysql://{aws_secrets['db_username']}:{aws_secrets['db_password']}@final-project-db-instance.ct0okikic8jc.eu-central-1.rds.amazonaws.com'
     db.init_app(app)
 
     from .views import views
@@ -38,4 +45,3 @@ def create_app():
         return User.query.get(id)
 
     return app
-    

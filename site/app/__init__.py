51156e3fea204d1b005a7c983ba46445e52a9b6d
Mantas Andriekus <mantas.andriekus@gmail.com>
2024-02-23 15:06:19 +0200

    Image for ecs. Testas kazkas negerai

--- site/app/__init__.py
+++ site/app/__init__.py
@@ -5,22 +5,15 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
-#def get_secrets():
-#    client = boto3.client('secretsmanager', region_name='eu-central-1')
-#    response = client.get_secret_value(SecretId='db_creds')
-#    secrets = json.loads(response['SecretString'])
-#    return secrets
+def get_secrets():
+    client = boto3.client('secretsmanager', region_name='eu-central-1')
+    response = client.get_secret_value(SecretId='db_creds')
+    secrets = json.loads(response['SecretString'])
+    return secrets
 # initialize SQLAlchemy object to
 # communication with database
 db = SQLAlchemy()
-#aws_secrets = get_secrets()
-aws_secrets = {
-    "db_username" : "FinALproJectuser01", 
-    "db_password" : "pT3a7Qtg17~kT95?kjlYes",
-    "db_host" : "final-project-db-instance.ct0okikic8jc.eu-central-1.rds.amazonaws.com",
-    "db_name" : "FinalProject"
-    }
-
+aws_secrets = get_secrets()
 
 def create_app():
     """ using onw method to create application"""

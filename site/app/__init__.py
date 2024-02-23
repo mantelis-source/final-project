968a2ace2d768a07b9123ff1d19bcc49c7559923
Mantas Andriekus <mantas.andriekus@gmail.com>
2024-02-23 16:12:56 +0200

    Test with sqlite db

--- site/app/__init__.py
+++ site/app/__init__.py
@@ -18,7 +18,7 @@ db = SQLAlchemy()
 def create_app():
     """ using onw method to create application"""
     app = Flask(__name__)
-    app.config["SECRET_KEY"] = aws_secrets['flask_secret_key']
+    app.config["SECRET_KEY"] = "sfdsfsfsfs" # aws_secrets['flask_secret_key']
 #    app.config["SQLALCHEMY_DATABASE_URI"] = \
 #    f'mysql+pymysql://{aws_secrets['db_username']}:{aws_secrets['db_password']}@{aws_secrets['db_host']}/{aws_secrets['db_name']}'
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///my.db"
